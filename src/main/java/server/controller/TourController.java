f46e0d7f52e7a67d6cf21e11f493ebc0ae2f321f
TheMadMike <m.gibas2407@gmail.com>
2022-01-06 18:32:01 +0100

    Add UPDATE /tours/new endpoint

--- src/main/java/server/controller/TourController.java
+++ src/main/java/server/controller/TourController.java
@@ -5,16 +5,21 @@ import server.service.TourService;
 import server.util.Controller;
 import server.util.Response;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class TourController extends Controller {
 
     TourService tourService = new TourService();
 
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @Override
     protected void initialize() {
         endpoints.put("GET all", this::getAll);
         endpoints.put("GET by_id", this::getById);
+        endpoints.put("UPDATE new", this::createNew);
     }
 
     // GET /tours/all
@@ -35,15 +40,35 @@ public class TourController extends Controller {
     // GET /tours/by_id <id>
     private void getById() {
         int id = Integer.parseInt(rawRequestBodyBuffer);
-        Tour tour = tourService.getById(id);
 
-        if(tour == null) {
-            respond(Response.Type.ERROR, "Tour not found\n");
-            return;
+        Tour tour = null;
+        try {
+            tour = tourService.getById(id);
+        } catch(RuntimeException exception) {
+            respond(Response.Type.ERROR, exception.getMessage()+"\n");
         }
 
         respond(tour.toCsv() + "\n");
     }
 
+
+    // UPDATE /tours/new <name>, <description>, <deprature date (dd/MM/yyyy)>, <max tourists>
+    private void createNew() {
+        String[] tokens = rawRequestBodyBuffer.split(",");
+
+        if(tokens.length != 4) {
+            respond(Response.Type.ERROR, "Invalid number of arguments\n");
+        }
+
+        String name = tokens[0].trim();
+        String description = tokens[1].trim();
+        int id = (name + description).hashCode();
+        LocalDate departureDate = LocalDate.parse(tokens[2].trim(), dateFormatter);
+        int maxTourists = Integer.parseInt(tokens[3].trim());
+
+        tourService.create(new Tour(id, name, description, departureDate, maxTourists));
+
+        ok();
+    }
 
 }
--- src/main/java/server/service/TourService.java
+++ src/main/java/server/service/TourService.java
@@ -17,9 +17,41 @@ public class TourService {
                 .collect(Collectors.toList());
     }
 
-    public Tour getById(int id) {
-        return getAll().stream()
-                .filter(tour -> tour.getId() == id)
-                .findFirst().orElse(null);
+    public Tour getById(int id) throws RuntimeException {
+        int index = findIndex(id);
+
+        return (Tour) toursRepository.read(index);
+    }
+
+    public void create(Tour tour) {
+        toursRepository.create(tour);
+    }
+
+    public void update(Tour tour) throws RuntimeException {
+        int index = findIndex(tour.getId());
+
+        toursRepository.update(index, tour);
+    }
+
+    public void remove(int id) throws RuntimeException {
+        int index = findIndex(id);
+
+        toursRepository.delete(index);
+    }
+
+    private int findIndex(int id) throws RuntimeException {
+        int index = -1;
+        for(int i = 0; i < getAll().size(); ++i) {
+            if(getAll().get(i).getId() == id) {
+                index = i;
+                break;
+            }
+        }
+
+        if(index == -1) {
+            throw new RuntimeException("Tour not found");
+        }
+
+        return index;
     }
 }
