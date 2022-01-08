f0e0f681d8f2fa2754d11631cec7870f90ad41f7
TheMadMike <m.gibas2407@gmail.com>
2022-01-08 19:48:18 +0100

    Add new endpoints in TourController: SET /tours/guide <tour id> <guide id> GET /tours/details <tour id>

--- src/main/java/server/controller/TourController.java
+++ src/main/java/server/controller/TourController.java
@@ -1,13 +1,16 @@
 package server.controller;
 
+import server.model.Guide;
 import server.model.Tour;
 import server.service.TourService;
 import server.util.Controller;
+import server.util.CsvParser;
 import server.util.Response;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TourController extends Controller {
 
@@ -20,6 +23,8 @@ public class TourController extends Controller {
         endpoints.put("GET all", this::getAll);
         endpoints.put("GET by_id", this::getById);
         endpoints.put("UPDATE new", this::createNew);
+        endpoints.put("SET guide", this::assignGuide);
+        endpoints.put("GET details", this::getDetails);
     }
 
     // GET /tours/all
@@ -54,21 +59,62 @@ public class TourController extends Controller {
 
     // UPDATE /tours/new <name>, <description>, <deprature date (dd/MM/yyyy)>, <max tourists>
     private void createNew() {
-        String[] tokens = rawRequestBodyBuffer.split(",");
+        List<String> tokens = CsvParser.parse(rawRequestBodyBuffer);
 
-        if(tokens.length != 4) {
+        if(tokens.size() != 4) {
             respond(Response.Type.ERROR, "Invalid number of arguments\n");
         }
 
-        String name = tokens[0].trim();
-        String description = tokens[1].trim();
+        String name = tokens.get(0);
+        String description = tokens.get(1);
         int id = (name + description).hashCode();
-        LocalDate departureDate = LocalDate.parse(tokens[2].trim(), dateFormatter);
-        int maxTourists = Integer.parseInt(tokens[3].trim());
+        LocalDate departureDate = LocalDate.parse(tokens.get(2), dateFormatter);
+        int maxTourists = Integer.parseInt(tokens.get(3));
 
         tourService.create(new Tour(id, name, description, departureDate, maxTourists));
 
         ok();
+    }
+
+    // SET /tours/guide <tour id>, <guide id>
+    private void assignGuide() {
+        List<String> tokens = CsvParser.parse(rawRequestBodyBuffer);
+
+        if(tokens.size() != 2) {
+            respond(Response.Type.ERROR, "Invalid number of arguments\n");
+        }
+
+        int tourId = Integer.parseInt(tokens.get(0));
+        int guideId = Integer.parseInt(tokens.get(1));
+
+        tourService.assignGuide(guideId, tourId);
+
+        ok();
+    }
+
+    // GET /tours/details <id>
+    private void getDetails() {
+        int id = Integer.parseInt(rawRequestBodyBuffer.trim());
+        Tour tour = tourService.getById(id);
+
+        StringBuffer buffer = new StringBuffer();
+
+        buffer.append("Tour name: ");
+        buffer.append(tour.getName());
+        buffer.append("\nDescription: ");
+        buffer.append(tour.getDescription());
+        buffer.append("\nDeparture date: ");
+        buffer.append(tour.getDepartureDate());
+        buffer.append("\nNumber of tourists: ");
+        buffer.append(tour.getTourists().size() + " / "+ tour.getMaxTourists());
+        buffer.append("\nTourists: ");
+        buffer.append(tour.getTourists());
+        buffer.append("\nGuide(s): ");
+        buffer.append(tourService.getGuidesForTour(tour.getId()).stream()
+                .map(Guide::getName).collect(Collectors.toList()));
+        buffer.append("\n");
+
+        respond(buffer.toString());
     }
 
 }
